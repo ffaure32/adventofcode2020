2bc4c1c193f7917b8b9768740be4fd42d1bbcb89
ffaure <frederic.faure@ext.cdiscount.com>
2020-12-03 09:30:15 +0100

    renames

--- day3/day3.py
+++ day3/day3.py
@@ -9,33 +9,33 @@ def cell_at(line, x_pos):
     return line[x_pos]
 
 
-def compute(lines):
-    return compute2(lines, (3, 1))
+def count_trees_on_standard_3_1_slope(map_lines):
+    return count_trees_on_slope(map_lines, (3, 1))
 
 
-def compute2(lines, step):
+def count_trees_on_slope(map_lines, slope):
     x = 0
     y = 0
     count_trees = 0
-    while y < len(lines)-1:
-        x += step[0]
-        y += step[1]
-        if y < len(lines):
-            cell = cell_at(lines[y], x)
+    while y < len(map_lines)-1:
+        x += slope[0]
+        y += slope[1]
+        if y < len(map_lines):
+            cell = cell_at(map_lines[y], x)
             if cell == TREE:
                 count_trees += 1
     return count_trees
 
 
 def solution(file_name):
-    return compute(prepare_data(file_name))
+    return count_trees_on_standard_3_1_slope(prepare_data(file_name))
 
 
 def solution2(file_name):
-    steps = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
-    lines = prepare_data(file_name)
-    computed_steps = [compute2(lines, step) for step in steps]
-    return math_utils.prod(computed_steps)
+    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    map_lines = prepare_data(file_name)
+    trees_on_slope = [count_trees_on_slope(map_lines, slope) for slope in slopes]
+    return math_utils.prod(trees_on_slope)
 
 
 def prepare_data(file_name):
