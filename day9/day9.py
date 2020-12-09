72fcba453df32e2b7ba8f6133cdcb5a9a1fead4a
ffaure <frederic.faure@ext.cdiscount.com>
2020-12-09 15:20:44 +0100

    mini-refacto

--- day9/day9.py
+++ day9/day9.py
@@ -22,8 +22,7 @@ def find_first_weakness(input_data, preamble_size):
 
 def prepare_data(file_name):
     lines = file_utils.get_lines("inputs", file_name)
-    int_lines = [int(line) for line in lines]
-    return int_lines
+    return [int(line) for line in lines]
 
 
 def solution(file_name, preamble_size):
@@ -35,12 +34,18 @@ def solution2(file_name, sum_to_find):
     prepared_data = prepare_data(file_name)
     index = prepared_data.index(sum_to_find)
     removed_tail = prepared_data[:index]
-    for i in range(index, 0, -1):
+    contiguous_range = find_contiguous_range(removed_tail, sum_to_find)
+    return min(contiguous_range)+max(contiguous_range)
+
+
+def find_contiguous_range(numbers_before_sum_to_find, sum_to_find):
+    for i in range(len(numbers_before_sum_to_find), 0, -1):
         current_sum = 0
         j = i-1
         while current_sum < sum_to_find:
-            current_sum += removed_tail[j]
+            current_sum += numbers_before_sum_to_find[j]
             j -= 1
+
         if current_sum == sum_to_find:
-            contiguous_range = removed_tail[j + 1:i]
-            return min(contiguous_range)+max(contiguous_range)
+            contiguous_range = numbers_before_sum_to_find[j + 1:i]
+            return contiguous_range
