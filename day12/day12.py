17f17d34cf76d0ffa9500c72949ee14a6daafb46
ffaure <frederic.faure@ext.cdiscount.com>
2020-12-12 22:35:22 +0100

    j'y connais rien en matrice de rotation

---
+++ day12/day12.py
@@ -0,0 +1,89 @@
+from utils import file_utils
+import math
+
+
+def prepare_data(file_name):
+    return file_utils.get_lines("inputs", file_name)
+
+
+def solution(file_name):
+    lines = prepare_data(file_name)
+    ship = Ship()
+    [ship.move(line) for line in lines]
+    return manhattan_distance(ship.position)
+
+
+def solution2(file_name):
+    lines = prepare_data(file_name)
+    ship = ShipWithWayPoint()
+    [ship.move(line) for line in lines]
+    return manhattan_distance(ship.position)
+
+
+DIRECTIONS = ['E', 'S', 'W', 'N']
+
+
+class Ship:
+    def __init__(self):
+        self.direction = 'E'
+        self.position = (0, 0)
+
+    def move(self, instruction):
+        command = instruction[:1]
+        value = int(instruction[1:])
+        if command == "R":
+            self.real_turn(value)
+        elif command == "L":
+            self.real_turn(-value)
+        elif command == 'F':
+            self.position = real_move(self.position, self.direction, value)
+        else:
+            self.position = real_move(self.position, command, value)
+
+    def real_turn(self, value):
+        rotation = value // 90
+        turn = DIRECTIONS.index(self.direction)
+        self.direction = DIRECTIONS[(turn + rotation) % len(DIRECTIONS)]
+
+
+class ShipWithWayPoint:
+    def __init__(self):
+        self.direction = 'E'
+        self.position = (0, 0)
+        self.way_point = (10, -1)
+
+    def move(self, instruction):
+        command = instruction[:1]
+        value = int(instruction[1:])
+        if command == "R":
+            self.real_turn(value)
+        elif command == "L":
+            self.real_turn(-value)
+        elif command == 'F':
+            self.position = (self.position[0] + self.way_point[0] * value,
+                            self.position[1] + self.way_point[1] * value)
+        else:
+            self.way_point = real_move(self.way_point, command, value)
+
+    def real_turn(self, value):
+        value = math.radians(value)
+        cos = math.cos(value)
+        sin = math.sin(value)
+        self.way_point = (cos * self.way_point[0] - sin * self.way_point[1],
+                          sin * self.way_point[0] + cos * self.way_point[1])
+
+
+def real_move(position, command, value):
+    if command == 'E':
+        position = (position[0] + value, position[1])
+    elif command == 'N':
+        position = (position[0], position[1] - value)
+    elif command == 'W':
+        position = (position[0] - value, position[1])
+    elif command == 'S':
+        position = (position[0], position[1] + value)
+    return position
+
+
+def manhattan_distance(position):
+    return round(abs(position[0])) + round(abs(position[1]))
---
+++ day12/test_day12.py
@@ -0,0 +1,38 @@
+import day12
+
+
+def test_init_ship():
+    ship = day12.Ship()
+    assert ship.direction == 'E'
+    assert ship.position == (0, 0)
+
+
+def test_front_move():
+    ship = day12.Ship()
+    ship.move("F10")
+    assert ship.direction == 'E'
+    assert ship.position == (10, 0)
+
+
+def test_solution():
+    result = day12.solution("test_1")
+    assert result == 25
+
+
+def test_solution_real():
+    result = day12.solution("input")
+    assert result == 508
+
+
+def test_solution2():
+    result = day12.solution2("test_1")
+    assert result == 286
+
+def test_solution22():
+    result = day12.solution2("test_2")
+    assert result == 332
+
+
+def test_solution2_real():
+    result = day12.solution2("input")
+    assert result == 30761
