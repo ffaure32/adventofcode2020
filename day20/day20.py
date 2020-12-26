3dcf060b89f188d2eddc2fa693e1f432783552e2
ffaure32 <ffaure32@yahoo.fr>
2020-12-26 19:09:35 +0100

    quelques deuxièmes étoiles pour se rappeler le ski de plus haut niveau

--- day20/day20.py
+++ day20/day20.py
@@ -1,3 +1,4 @@
+import math
 import re
 
 from utils import file_utils, math_utils
@@ -10,7 +11,7 @@ class Tile:
         self.id = id
         self.lines = []
         self.sides = []
-        self.neighbours = set()
+        self.neighbours = dict()
 
     def add_line(self, line):
         self.lines.append(line)
@@ -30,9 +31,27 @@ class Tile:
             self.sides.append(self.sides[i][::-1])
 
     def has_common_side(self, other_tile):
-        if any(side in other_tile.sides for side in self.sides):
-            self.neighbours.add(other_tile)
-            other_tile.neighbours.add(self)
+        common_side = set(self.sides).intersection(other_tile.sides)
+        if common_side:
+            common_side = next(iter(common_side))
+            self.neighbours[other_tile] = self.sides.index(common_side)
+            other_tile.neighbours[self] = other_tile.sides.index(common_side)
+
+    def rotate(self):
+        self.lines = list(zip(*self.lines[::-1]))
+
+    def flip_horizontally(self):
+        self.lines = self.lines[::-1]
+
+    def flip_vertically(self):
+        new_lines = list()
+        for line in self.lines:
+            line = line[::-1]
+            new_lines.append(line)
+        self.lines = new_lines
+
+    def print(self):
+        [print(line) for line in self.lines]
 
 
 tile_regex = re.compile(r'Tile (\d*):')
@@ -50,20 +69,80 @@ def prepare_data(file_name):
             tiles.append(current_tile)
         else:
             current_tile.add_line(line)
+    init_neighbours(tiles)
     return tiles
+
+
+def init_neighbours(tiles):
+    for i in range(len(tiles)):
+        for j in range(i + 1, len(tiles)):
+            tiles[i].has_common_side(tiles[j])
 
 
 def solution(file_name):
     tiles = prepare_data(file_name)
-    for i in range(len(tiles)):
-        for j in range(i + 1, len(tiles)):
-            tiles[i].has_common_side(tiles[j])
     corner_tiles = list(filter(lambda tile: len(tile.neighbours) == 2, tiles))
     result = [tile.id for tile in corner_tiles]
     print(math_utils.prod(result))
     return result
 
 
+def solution2(file_name):
+    tiles = prepare_data(file_name)
+    matrix_size = round(math.sqrt(len(tiles)))
+    tiles_matrix = list()
+    for i in range(matrix_size):
+        tiles_matrix.append(list())
+    corner_tiles = list(filter(lambda tile: len(tile.neighbours) == 2, tiles))
+    edge_tiles = list(filter(lambda tile: len(tile.neighbours) <= 3, tiles))
+    next_tile = corner_tiles[0]
+    next_tile.rotate()
+    next_tile.rotate()
+    edge_tiles.remove(next_tile)
+    tiles.remove(next_tile)
+    tiles_matrix[0].append(next_tile)
+    print()
+    # first line
+    for i in range(1, matrix_size):
+        previous_tile = next_tile
+        next_tile = list(filter(lambda tile: tile in next_tile.neighbours.keys(), edge_tiles))[0]
+        previous_tile.print()
+        print(previous_tile.neighbours[next_tile])
+        print(next_tile.neighbours[previous_tile])
+        to_rotate = next_tile.neighbours[previous_tile]
+        if to_rotate == 3:
+            next_tile.rotate()
+            next_tile.rotate()
+        tiles_matrix[0].append(next_tile)
+        edge_tiles.remove(next_tile)
+        tiles.remove(next_tile)
+    # first column
+    for i in range(1, matrix_size):
+        next_tile = list(filter(lambda tile: tile in tiles_matrix[i - 1][0].neighbours.keys(), edge_tiles))[0]
+        tiles_matrix[i].append(next_tile)
+        edge_tiles.remove(next_tile)
+        tiles.remove(next_tile)
+    # inside tiles
+    for i in range(1, matrix_size):
+        for j in range(1, matrix_size):
+            next_tile = list(filter(
+                lambda tile: tile in tiles_matrix[i - 1][j].neighbours.keys() and tile in tiles_matrix[i][
+                    j - 1].neighbours.keys(), tiles))[0]
+            tiles_matrix[i].append(next_tile)
+            tiles.remove(next_tile)
+
+    for i in range(0, matrix_size):
+        for j in range(0, matrix_size):
+            tiles_matrix[j][i].print()
+            print()
+    return tiles_matrix
+
+
 def test_solution():
     result = solution('input')
+    assert 51214443014783 == math_utils.prod(result)
+
+
+def test_solution2():
+    result = solution2('input')
     assert 51214443014783 == math_utils.prod(result)
